d3b8b738a58498bdd10eee05266af9cb6939d642
Mayank77maruti <mayank.mohapatra7@gmail.com>
2025-12-24 11:28:35 +0530

    ce

--- src/commands/texts.rs
+++ src/commands/texts.rs
@@ -1,26 +1,49 @@
-pub const HELP: &str = r#"<span class="grn"> _____________  __  ___________  __   ________ __________
-.___  ___.      ___   ____    ____  ___      .__   __.  __  ___ 
-|   \/   |     /   \  \   \  /   / /   \     |  \ |  | |  |/  / 
-|  \  /  |    /  ^  \  \   \/   / /  ^  \    |   \|  | |  '  /  
-|  |\/|  |   /  /_\  \  \_    _/ /  /_\  \   |  . `  | |    <   
-|  |  |  |  /  _____  \   |  |  /  _____  \  |  |\   | |  .  \  
-|__|  |__| /__/     \__\  |__| /__/     \__\ |__| \__| |__|\__\  
-
-            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀
-            ⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣷⣦⣄⣀⣤⣶⣶
-            ⠀⠀⠀⠀⠀⣰⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⠟⠋
-            ⠀⠀⠀⠀⣼⣿⡿⠃⠀⢀⣤⣾⣿⣿⣿⣿⣷⣦⣄⠀⠀⠈⠉⠀⠀⠀
-            ⠀⠀⠀⣸⣿⡿⠁⠀⢠⣿⣿⠟⠉⠀⠈⠉⠛⢿⣿⣷⡄⠀⠀⠀⠀⠀
-            ⠀⠀⢀⣿⣿⡇⠀⠀⣾⣿⡟⠀⠀⢀⣤⣄⠀⠀⠹⣿⣿⡄⠀⠀⠀⠀
-            ⠀⠀⣾⣿⣿⡇⠀⠀⢻⣿⣷⡀⠀⠘⣿⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀
-            ⠀⣼⣿⡿⣿⣿⡄⠀⠈⠻⣿⣿⣷⣿⣿⡿⠃⠀⢀⣿⣿⡇⠀⠀⠀⠀
-            ⣰⣿⣿⠁⠹⣿⣿⣦⡀⠀⠈⠉⠛⠋⠉⠀⠀⣠⣾⣿⡟⠀⠀⠀⠀⠀
-            ⣿⣿⣧⣤⣤⣬⣿⣿⣿⣶⣦⣤⣤⣤⣴⣶⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀
-            ⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+pub const HELP: &str = r#"<span class="grn">
 </span>
 
-Hello World
-Type <i>help</i> to see the list of available commands
+<table>
+  <tr>
+    <td><span class="rd semibold">about</span></td>
+    <td>View about me</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">neofetch / fastfetch / github</span></td>
+    <td>View GitHub profile</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">onefetch / repos</span></td>
+    <td>View pinned repos / projects</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">links</span></td>
+    <td>View contact info and links</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">help</span></td>
+    <td>View this help section</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">theme / wal</span></td>
+    <td>Cycle through themes</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">credits</span></td>
+    <td>View credits and repository</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">history</span></td>
+    <td>View command history</td>
+  </tr>
+  <tr>
+    <td><span class="rd semibold">clear</span></td>
+    <td>Clear screen</td>
+  </tr>
+</table>
+
+<br />
+
+You can use <i>arrow keys</i> to scroll through history,<br />
+and <i>Ctrl + L</i> to clear the screen.<br /><br />
 "#;
 
 pub const CREDITS: &str = r#"<span class="grn"> _____________  __  ___________  __   ________
@@ -30,9 +53,9 @@ pub const CREDITS: &str = r#"<span class="grn"> _____________  __  ___________  
 </span>
 Terminal style portfolio website. 
  
-  <a href="https://github.com/Mayank77maruti" target="_blank" class="blu semibold">Github</a>: github.com/Mayank77maruti
+  <a href="https://github.com/shettysach" target="_blank" class="blu semibold">Github</a>: github.com/shettysach
 
-  <a href="https://github.com/Mayank77maruti/termfolio" target="_blank" class="blu semibold">Repo</a>: github.com/Mayank77maruti/termfolio
+  <a href="https://github.com/shettysach/termfolio" target="_blank" class="blu semibold">Repo</a>: github.com/shettysach/termfolio
 
 <span class="rd semibold">APIs used -</span>
 
